f4ba9e607d054d7656c9f2744e343b728667cc50
VictoriaMakaryan <138680813+VictoriaMakaryan@users.noreply.github.com>
2024-02-23 16:54:22 +0400

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    float b = 2.3;
+    char c = 'k';
+    double d = 3.14159;
+    bool myBool = true;
+
+    std::cout << a << "\n"; 
+    std::cout << b << "\n";
+    std::cout << c << "\n";
+    std::cout << d << "\n";
+    std::cout << myBool << "\n";
+}
